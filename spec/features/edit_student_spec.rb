a61dd5df7b7cc6a30bda5f9c38d14c971f0736c9
Burke Fielder <burke.fielder@gmail.com>
2016-03-17 13:26:25 -0700

    Refactor feature specs for edit form

--- spec/features/edit_student_spec.rb
+++ spec/features/edit_student_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 describe 'Editing an individual student' do
 
-  it 'shows the student details' do
+  it 'updates the student details and shows the updated details' do
     student = Student.create(student_attributes)
 
-    visit edit_student_url(student)
+    visit student_url(student)
 
-    expect(page).to have_text(student.first_name)
-    expect(page).to have_text(student.last_name)
-    expect(page).to have_text('Triennial due')
-    expect(page).to have_text('Iep date')
-    expect(page).to have_text('IEP Minutes')
+    click_link 'Edit'
+
+    expect(current_path).to eq(edit_student_path(student))
+    expect(find_field('First name').value).to eq(student.first_name)
+    expect(find_field('Last name').value).to eq(student.last_name)
+    expect(find('input[type=checkbox]').value).to eq '1'
+    expect(find_field('Iep date').value).to eq(student.iep_date.strftime('%F'))
+    expect(find('input[type=number]').value).to eq(student.min_hours.to_s)
   end
 end
