9cf37fc8ce2051f6def2f5945a89014781afb993
Burke Fielder <burke.fielder@gmail.com>
2016-03-28 14:22:13 -0700

    Add specs for changes to view

--- spec/features/student/show_student_spec.rb
+++ spec/features/student/show_student_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Viewing an individual student' do
+describe 'Viewing an individual student with tri and dismissed' do
   it 'shows the student details' do
     student = Student.create(student_attributes)
 
@@ -9,11 +9,23 @@ describe 'Viewing an individual student' do
     expect(page).to have_text(student.first_name)
     expect(page).to have_text(student.last_name)
     expect(page).to have_text(student.iep_date.strftime('%F'))
-    expect(page).to have_text(student.triennial)
+    expect(page).to have_css('i.fi-check')
     expect(page).to have_text(student.min_hours)
     expect(page).to have_text(student.years_old(student.age))
     expect(page).to have_text(student.grade)
     expect(page).to have_text(student.diagnosis)
-    expect(page).to have_text(student.dismissed)
+    expect(page).to have_css('i.fi-check')
+  end
+end
+
+describe 'Viewing an individual student that does not have a tri' do
+  it 'shows the prohibited symbol' do
+    student = Student.create(student_attributes)
+    student.triennial = false
+    student.save
+
+    visit student_url(student)
+
+    expect(page).to have_css('i.fi-prohibited')
   end
 end
