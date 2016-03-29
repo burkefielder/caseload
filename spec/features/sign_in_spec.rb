92bf83bf6d33096da1552c91a6fba485f6dcba05
Burke Fielder <burke.fielder@gmail.com>
2016-03-29 13:56:11 -0700

    Add user sign in functionality with specs

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe 'Signing in' do
+  it 'prompts for an email and password' do
+    visit root_url
+
+    click_link 'Login'
+
+    expect(page).to have_css('input#email')
+    expect(page).to have_css('input#password')
+  end
+
+  it 'signs in the user if the email/password combination is valid' do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link 'Login'
+
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
+
+    click_button 'Login'
+
+    expect(current_path).to eq(user_path(user))
+
+    expect(page).to have_text("Welcome back, #{user.name}!")
+  end
+
+  it 'does not sign in the user if the email/password combination is invalid' do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link 'Login'
+
+    fill_in 'email', with: user.email
+    fill_in 'password', with: 'no match'
+
+    click_button 'Login'
+
+    expect(page).to have_text('Invalid')
+  end
+end
