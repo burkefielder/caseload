feebd5cb07701ab9c84fe75f4309e17f8ac91767
Burke Fielder <burke.fielder@gmail.com>
2016-03-29 16:59:20 -0700

    Remove serializer for production

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,4 +1,4 @@
-class SessionSerializer < ActiveModel::Serializer
-  attributes :id, :date, :minutes, :notes, :therapy
-  has_one :student
-end
+# class SessionSerializer < ActiveModel::Serializer
+#   attributes :id, :date, :minutes, :notes, :therapy
+#   has_one :student
+# end
