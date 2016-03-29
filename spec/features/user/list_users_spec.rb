36b246f2de92f73de43bfd233a749156e8c4393c
Burke Fielder <burke.fielder@gmail.com>
2016-03-29 10:51:13 -0700

    Modify list_users_spec

--- spec/features/user/list_users_spec.rb
+++ spec/features/user/list_users_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-describe "Viewing the list of users" do
-
-  it "shows the users" do
-    user1 = User.create!(user_attributes(name: "Larry", email: "larry@example.com"))
-    user2 = User.create!(user_attributes(name: "Moe",   email: "moe@example.com"))
-    user3 = User.create!(user_attributes(name: "Curly", email: "curly@example.com"))
+describe 'Viewing the list of users' do
+  it 'shows the users' do
+    user1 = User.create!(user_attributes(name: 'Larry', email: 'larry@example.com'))
+    user2 = User.create!(user_attributes(name: 'Moe',   email: 'moe@example.com'))
+    user3 = User.create!(user_attributes(name: 'Curly', email: 'curly@example.com'))
 
     visit users_url
 
@@ -13,5 +12,4 @@ describe "Viewing the list of users" do
     expect(page).to have_link(user2.name)
     expect(page).to have_link(user3.name)
   end
-
 end
